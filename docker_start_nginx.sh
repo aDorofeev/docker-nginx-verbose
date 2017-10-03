beced101a6dd64edd6e6b68ba48978831ea792b2
Anton Dorofeev <dorofeev@complisoft.ru>
2017-10-03 18:02:53 +0300

    [bugfix] sighup wasn't forwarded to nginx process

--- docker_start_nginx.sh
+++ docker_start_nginx.sh
@@ -2,16 +2,4 @@
 
 /usr/local/bin/make_error_pages.sh
 
-_term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$CHILD_NGINX_PID"
-}
-
-trap _term SIGTERM
-
-echo "Starging nginx...";
-nginx &
-
-CHILD_NGINX_PID=$!
-wait "$CHILD_NGINX_PID"
-echo "Nginx terminated."
+exec nginx
