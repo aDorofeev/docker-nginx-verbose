5845e7b94c840afad362b7f40e1e37b32214a93e
Anton Dorofeev <anton@dorofeev.me>
2018-10-14 16:06:17 +0200

    [bugfix] nginx should be launched from entrypoint directly, signals won't work otherwise

--- docker/nginx/enable-sites.sh
+++ docker/nginx/enable-sites.sh
@@ -10,5 +10,3 @@ find /etc/nginx/sites-available/ -name '*.nginx.tpl' | \
 find /etc/nginx/sites-available/ -name '*.nginx' | \
   xargs -i bash -c \
     'FILENAME=$(basename {}); cd /etc/nginx/sites-enabled; rm -f "${FILENAME}"; ln -s "../sites-available/${FILENAME}" "${FILENAME}"'
-
-exec nginx
